5cbe50b41d74aea34b4c4d94cc6cf888ec56aeee
rimonaldo <rimonsade87@gmail.com>
2025-06-24 21:42:12 -0400

    Refactor comments  in main.c

--- src/main.c
+++ src/main.c
@@ -6,43 +6,62 @@
 
 int main(int argc, char *argv[])
 {
-    /* Check if input file argument is provided */
+    /* Check if input file was provided */
     if (argc < 2)
     {
+        /* Print usage message and exit with error code */
         fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
         return 1;
     }
 
-    /* Get the input filename from command line */
+    /* Get pointer to input filename from command-line argument */
     const char *input_filename = argv[1];
 
-    /* Extract the base name (file name without path) */
+    /*
+     * Extract the basename of the file (without path):
+     * If input is "path/to/file.asm", we want just "file.asm"
+     * strrchr finds the last '/' in the string
+     */
     const char *basename = strrchr(input_filename, '/');
-    basename = (basename != NULL) ? basename + 1 : input_filename;
+    if (basename != NULL)
+        basename++; /* Skip the '/' */
+    else
+        basename = input_filename;
 
-    /* Prepare the expanded filename for the output of the preprocessor */
+    /*
+     * Prepare destination buffer for the expanded file name:
+     * This will become: "output/file.am"
+     */
     char expanded_filename[1024];
     generate_expanded_filename(expanded_filename, sizeof(expanded_filename), basename);
 
-    /* Run the pre-assembler stage */
+    /* Run the pre-assembler on the original source file */
     run_pre_assembler(input_filename);
 
-    /* Run the first pass stage on the expanded file */
+    /* Run the first pass on the preprocessed (".am") file */
     run_first_pass(expanded_filename);
 
+    /* Return success */
     return 0;
 }
 
-/* @brief
- * Generate the expanded filename by:
- * - Placing it in the "output/" directory
- * - Using the base name of the input file, but removing its extension (if any)
- * - Appending the ".am" extension
- * For example, "source.asm" becomes "output/source.am"
+/*
+ * Build the output file name with extension ".am" in the "output/" folder.
+ * - 'dest' is the destination buffer
+ * - 'dest_size' is the size of that buffer
+ * - 'basename' is a filename like "file.asm" (not including directory)
+ *
+ * The function removes the extension and adds ".am" instead.
  */
 void generate_expanded_filename(char *dest, size_t dest_size, const char *basename)
 {
-
+    /*
+     * strrchr(basename, '.') returns pointer to last dot in filename
+     * If there is a dot, we truncate before it.
+     * Else, we take full length of basename.
+     *
+     * snprintf ensures we do not overflow the destination buffer.
+     */
     snprintf(dest, dest_size, "output/%.*s.am",
              (int)(strrchr(basename, '.') ? strrchr(basename, '.') - basename : strlen(basename)),
              basename);
