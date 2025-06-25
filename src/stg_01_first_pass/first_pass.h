f0977460c3902070b566faa9387ba3f72ec68b9c
rimonaldo <rimonsade87@gmail.com>
2025-06-25 12:32:12 -0400

    Add function declarations for instruction and directive parsing

--- src/stg_01_first_pass/first_pass.h
+++ src/stg_01_first_pass/first_pass.h
@@ -1,6 +1,13 @@
 #ifndef FIRST_PASS_H
 #define FIRST_PASS_H
+#include "../common/AST/ast.h"
+#include "../common/tokenizer/tokenizer.h"
 
 void run_first_pass(char *filename);
+ASTNode *parse_instruction_line(int line_num, int DC, Tokens tokenized_line);
+ASTNode *parse_directive_line(int line_num, Tokens tokenized_line);
+int is_label_declare(char *token);
+int is_directive_line(Tokens tokenized_line);
+AddressingMode get_mode(Tokens tokenized_line, int token_idx);
 
 #endif
