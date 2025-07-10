62f62013ff6002417289be6fa389f61b3dccd687
rimonaldo <rimonsade87@gmail.com>
2025-07-10 16:24:52 -0400

    Defined label related error codes

--- src/common/errors/errors.h
+++ src/common/errors/errors.h
@@ -47,7 +47,7 @@ typedef enum
 
     /* Memory Errors (Assembly stops if violated) */
     MEM_E201_WRD_LIM = 301,
-    MEM_E202_LN_LIM,
+    MEM_E202_LINE_CHAR_LIMIT,
     MEM_E203_SO_RSK,
     MEM_W201_UNSD
 } ErrorCode;
@@ -74,3 +74,46 @@ const ErrorInfo write_error_log(StatusInfo *status_info, ErrorCode code, int lin
 void free_status_info(StatusInfo *status_info);
 
 #endif
+
+typedef enum
+{
+    /* Label Errors */
+    E501_LABEL_INVALID = 501,
+    E502_LABEL_RESERVED,
+    E503_LABEL_REDEFINED,
+    E504_LABEL_UNDEFINED,
+    E505_LABEL_FORWARD_UNDEFINED,
+    E506_LABEL__ENTRY_NOT_FOUND,
+    E507_LABEL_ENTRY_AND_EXTERN,
+    W508_LABEL_OR_ENTRY_OR_EXTERN,
+
+    /* Semantic Errors (Critical - halt assembly) */
+    E101 = 101,
+    E102,
+    E103,
+    E104,
+    E105,
+    E106,
+    E107,
+    E108,
+    E109,
+    E110,
+    E111,
+    E112,
+
+    /* Warnings (Non-critical - allow assembly) */
+    W001 = 201,
+    W002,
+    W003,
+    W004,
+    W005,
+    W006,
+    W007,
+    W008,
+
+    /* Memory Errors (Assembly stops if violated) */
+    MEM_E201_WRD_LIM = 301,
+    MEM_E202_LINE_CHAR_LIMIT,
+    MEM_E203_SO_RSK,
+    MEM_W201_UNSD
+} ErrorCode2;
